95bcd9a461b69cd66a8d1d8ddde87e232ac48706
DaRock <mariuz.felea@gmail.com>
2026-01-15 16:50:51 +0200

    small fix

--- catalog/controller/extension/module/ovesio_ecommerce.php
+++ catalog/controller/extension/module/ovesio_ecommerce.php
@@ -2,9 +2,9 @@
 
 ini_set('serialize_precision', -1);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 ini_set('memory_limit', '2G');
 
 class ControllerExtensionModuleOvesioEcommerce extends Controller {
